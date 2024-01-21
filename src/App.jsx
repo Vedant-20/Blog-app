1400e9edb9fae8b40100c0a3965d60f81a6a6098
Vedant <vedantwarjurkar@gmail.com>
2024-01-21 08:36:48 +0530

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
+import { useEffect, useState } from "react"
+import { useDispatch } from "react-redux"
+import authService from './appwrite/auth'
+import {login,logout} from './store/authSlice'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import { Outlet } from "react-router-dom"
 
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  const [loading, setLoading]=useState(true)
+  const dispatch=useDispatch()
   
-
-  return (
-    <>
-      <h1>Blog App with Appwrite</h1>
-    </>
-  )
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(()=>setLoading(false))
+  },[])
+  return !loading ? (<div className="min-h-screen flex flex-wrap content-between bg-gray-500">
+      <div className="w-full block ">
+        <Header/>
+          <main>
+            {/* <Outlet/> */}
+          </main>
+        <Footer/>
+      </div>
+  </div>) : null
 }
 
 export default App
---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Logo = ({width= '100 px'}) => {
+  return (
+    <div>Logo</div>
+  )
+}
+
+export default Logo
